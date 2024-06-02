1f0ad18ea66c1e9ae880756cb08baa69d2941060
Pooja <pooja.gaware0310@gmail.com>
2024-06-02 21:12:16 +0530

    localstorage

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route ,Redirect} from 'react-router-dom';
 
 import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
+import { useContext } from 'react';
+import AuthContext from './store/auth-context';
 
 function App() {
+
+const authCnt=useContext(AuthContext);
+
   return (
     <Layout>
       <Switch>
         <Route path='/' exact>
           <HomePage />
         </Route>
-        <Route path='/auth'>
+        {!authCnt.isLoggedIn && (<Route path='/auth'>
           <AuthPage />
-        </Route>
+        </Route>)}
         <Route path='/profile'>
-          <UserProfile />
+        {authCnt.isLoggedIn && <UserProfile /> }
+        {!authCnt.isLoggedIn && <Redirect to='/auth'/>}
+          </Route>
+        <Route path='*'>
+          <Redirect to='/'/>
         </Route>
       </Switch>
     </Layout>
--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,37 +1,40 @@
 import React, { useState } from "react";
 
-const AuthContext=React.createContext({
-    token:'',
-    isLoggedIn:false,
-    login:(token)=>{},
-    logout:()=>{},
+const AuthContext = React.createContext({
+  token: "",
+  isLoggedIn: false,
+  login: (token) => {},
+  logout: () => {},
 });
 
-export const AuthContextProvider=(props)=>{
-const [token,setToken]=useState(null);
+export const AuthContextProvider = (props) => {
+    const initialToken=localStorage.getItem('token')
+  const [token, setToken] = useState(initialToken);
 
-const isUserLoggedIn=!!token;
+  const isUserLoggedIn = !!token;
 
-const loginHandler=(token)=>{
+  const loginHandler = (token) => {
     setToken(token);
-};
+    localStorage.setItem("token", token);
+  };
 
-const logoutHandler=()=>{
+  const logoutHandler = () => {
     setToken(null);
+    localStorage.removeItem("token");
+  };
+
+  const contextValue = {
+    token: token,
+    isLoggedIn: isUserLoggedIn,
+    login: loginHandler,
+    logout: logoutHandler,
+  };
+
+  return (
+    <AuthContext.Provider value={contextValue}>
+      {props.children}
+    </AuthContext.Provider>
+  );
 };
-
-const contextValue={
-    token:token,
-    isLoggedIn:isUserLoggedIn,
-    login:loginHandler,
-    logout:logoutHandler,
-}
-
-    return(
-        <AuthContext.Provider value={contextValue}>
-            {props.children}
-        </AuthContext.Provider>
-    )
-}
 
 export default AuthContext;
